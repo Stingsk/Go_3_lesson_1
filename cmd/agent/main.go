dee74515018d40e243c1b26acf1965cf717d2c75
sting.sk <s.volkov@gateline.ru>
2022-02-05 22:57:18 +0300

    Test metrics

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,53 @@
 package main
 
-func main() {}
+import (
+	"bytes"
+	"fmt"
+	metrics "github.com/Stingsk/Go_3_lesson_1/internal/metrics"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	endpoint := "http://localhost:8080/setMetrics/"
+
+	monitor, err := metrics.NewMonitor()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// конструируем HTTP-клиент
+	client := &http.Client{}
+	// конструируем запрос
+	// запрос методом POST должен, кроме заголовков, содержать тело
+	// тело должно быть источником потокового чтения io.Reader
+	// в большинстве случаев отлично подходит bytes.Buffer
+	request, err := http.NewRequest(http.MethodPost, endpoint, bytes.NewReader(monitor))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// в заголовках запроса сообщаем, что данные кодированы стандартной URL-схемой
+	request.Header.Add("Content-Type", "application/json; charset=UTF-8")
+	request.Header.Add("Content-Length", strconv.Itoa(len(monitor)))
+	// отправляем запрос и получаем ответ
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// печатаем код ответа
+	fmt.Println("Статус-код ", response.Status)
+	defer response.Body.Close()
+	// читаем поток из тела ответа
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// и печатаем его
+	fmt.Println(string(body))
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
-	metrics "github.com/Stingsk/Go_3_lesson_1/internal/metrics"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
+func setMetrics(w http.ResponseWriter, r *http.Request) {
+	// этот обработчик принимает только запросы, отправленные методом GET
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only GET requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+	// читаем Body
+	b, err := io.ReadAll(r.Body)
+	// обрабатываем ошибку
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	fmt.Println(b)
+}
+
 func main() {
-	go metrics.NewMonitor(1000 * 1000 * 10)
-	http.HandleFunc("/GetMetrics/", metrics.GetMetrics)
+	http.HandleFunc("/setMetrics/", setMetrics)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }

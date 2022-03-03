ca98ae368932cd9a403ad985cfda95c369bce244
sting.sk <s.volkov@gateline.ru>
2022-03-03 23:52:08 +0300

    fix tests

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,9 +18,9 @@ import (
 
 type config struct {
 	Address       string        `env:"ADDRESS" envDefault:"localhost:8080"`
-	StoreInterval time.Duration `env:"STORE_INTERVAL" envDefault:"0s"`
+	StoreInterval time.Duration `env:"STORE_INTERVAL" envDefault:"300s"`
 	StoreFile     string        `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
-	Restore       bool          `env:"RESTORE" envDefault:"false"`
+	Restore       bool          `env:"RESTORE" envDefault:"true"`
 }
 
 func main() {

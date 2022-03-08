dd73f938f7c5bcabfebe748cf79ae060238c5e3c
sting.sk <s.volkov@gateline.ru>
2022-03-08 16:58:48 +0300

    get paramets from flag

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -10,24 +10,34 @@ import (
 
 	"github.com/caarlos0/env/v6"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 
 	"github.com/Stingsk/Go_3_lesson_1/internal/httputil"
 	"github.com/Stingsk/Go_3_lesson_1/internal/logs"
 	"github.com/Stingsk/Go_3_lesson_1/internal/metrics"
 )
 
+const (
+	defaultServerAddress  = "localhost:8080"
+	defaultReportInterval = 10 * time.Second
+	defaultPollInterval   = 2 * time.Second
+)
+
 type config struct {
-	Address        string        `env:"ADDRESS" envDefault:"localhost:8080"`
-	ReportInterval time.Duration `env:"REPORT_INTERVAL" envDefault:"10s"`
-	PollInterval   time.Duration `env:"POLL_INTERVAL" envDefault:"2s"`
+	Address        string
+	ReportInterval time.Duration
+	PollInterval   time.Duration
+}
+
+type configFromEVN struct {
+	Address        string `env:"ADDRESS" envDefault:"notset"`
+	ReportInterval string `env:"REPORT_INTERVAL" envDefault:"notset"`
+	PollInterval   string `env:"POLL_INTERVAL" envDefault:"notset"`
 }
 
 func main() {
 	logs.Init()
-	cfg := config{}
-	if err := env.Parse(&cfg); err != nil {
-		logrus.Error(err)
-	}
+	cfg := getConfig()
 	logrus.Debug("Start agent")
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan,
@@ -53,4 +63,48 @@ func main() {
 
 	wg.Wait()
 	logrus.Debug("Shutdown agent")
+}
+
+func getConfig() config {
+	cfg := config{}
+	configEVN := configFromEVN{}
+	if err := env.Parse(&configEVN); err != nil {
+		logrus.Error(err)
+	}
+	rootCmd := &cobra.Command{
+		Use:   "agent",
+		Short: "Metrics for Agent",
+		Long:  "Metrics for Agent",
+	}
+
+	if configEVN.Address == "notset" {
+		rootCmd.Flags().StringVarP(&cfg.Address, "address", "a", defaultServerAddress,
+			"Pair of host:port to send data")
+	} else {
+		cfg.Address = configEVN.Address
+	}
+
+	if configEVN.ReportInterval == "notset" {
+		rootCmd.Flags().DurationVarP(&cfg.ReportInterval, "reportInterval", "r", defaultReportInterval,
+			"Seconds to periodically save metrics")
+	} else {
+		var err error
+		cfg.ReportInterval, err = time.ParseDuration(configEVN.ReportInterval)
+		if err != nil {
+			cfg.ReportInterval = defaultReportInterval
+		}
+	}
+
+	if configEVN.PollInterval == "notset" {
+		rootCmd.Flags().DurationVarP(&cfg.PollInterval, "pollInterval", "p", defaultPollInterval,
+			"Seconds to periodically send metrics to server")
+	} else {
+		var err error
+		cfg.PollInterval, err = time.ParseDuration(configEVN.PollInterval)
+		if err != nil {
+			cfg.PollInterval = defaultPollInterval
+		}
+	}
+
+	return cfg
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"os/signal"
+	"strconv"
 	"sync"
 	"syscall"
 	"time"
@@ -11,26 +12,36 @@ import (
 	"github.com/Stingsk/Go_3_lesson_1/internal/storage"
 	"github.com/caarlos0/env/v6"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 
 	"github.com/Stingsk/Go_3_lesson_1/internal/httputil"
 	"github.com/Stingsk/Go_3_lesson_1/internal/logs"
 )
 
+const (
+	defaultServerAddress = "localhost:8080"
+	defaultStoreFile     = "/tmp/devops-metrics-db.json"
+	defaultStoreInterval = 300 * time.Second
+)
+
 type config struct {
-	Address       string        `env:"ADDRESS" envDefault:"localhost:8080"`
-	StoreInterval time.Duration `env:"STORE_INTERVAL" envDefault:"0s"`
-	StoreFile     string        `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
-	Restore       bool          `env:"RESTORE" envDefault:"true"`
+	Address       string        `env:"ADDRESS"`
+	StoreInterval time.Duration `env:"STORE_INTERVAL"`
+	StoreFile     string        `env:"STORE_FILE"`
+	Restore       bool          `env:"RESTORE"`
+}
+
+type configFromEVN struct {
+	Address       string `env:"ADDRESS" envDefault:"notset"`
+	StoreInterval string `env:"STORE_INTERVAL" envDefault:"notset"`
+	StoreFile     string `env:"STORE_FILE" envDefault:"notset"`
+	Restore       string `env:"RESTORE" envDefault:"notset"`
 }
 
 func main() {
 	logs.Init()
-	cfg := config{}
-	if err := env.Parse(&cfg); err != nil {
-		logrus.Error(err)
-	}
 	var metricData storage.MetricResourceMap
-
+	cfg := getConfig()
 	if cfg.Restore {
 		logrus.Info("Load data from " + cfg.StoreFile)
 		metricRead, _ := file.ReadMetrics(cfg.StoreFile)
@@ -55,4 +66,55 @@ func main() {
 	wg.Wait()
 
 	logrus.Debug("Shutdown server")
+}
+
+func getConfig() config {
+	cfg := config{}
+	configEVN := configFromEVN{}
+	if err := env.Parse(&configEVN); err != nil {
+		logrus.Error(err)
+	}
+	rootCmd := &cobra.Command{
+		Use:   "server",
+		Short: "Metrics for Server",
+		Long:  "Metrics for Server",
+	}
+
+	if configEVN.Address == "notset" {
+		rootCmd.Flags().StringVarP(&cfg.Address, "address", "a", defaultServerAddress,
+			"Pair of host:port to listen on")
+	} else {
+		cfg.Address = configEVN.Address
+	}
+
+	if configEVN.StoreFile == "notset" {
+		rootCmd.Flags().StringVarP(&cfg.StoreFile, "file", "f", defaultStoreFile,
+			"Path to save metrics")
+	} else {
+		cfg.StoreFile = configEVN.StoreFile
+	}
+
+	if configEVN.Restore == "notset" {
+		rootCmd.Flags().BoolVarP(&cfg.Restore, "restore", "r", true,
+			"Flag to load initial metrics from storage ")
+	} else {
+		var err error
+		cfg.Restore, err = strconv.ParseBool(configEVN.Restore)
+		if err != nil {
+			cfg.Restore = true
+		}
+	}
+
+	if configEVN.StoreInterval == "notset" {
+		rootCmd.Flags().DurationVarP(&cfg.StoreInterval, "interval", "i", defaultStoreInterval,
+			"Seconds to periodically save metrics if 0 save immediately")
+	} else {
+		var err error
+		cfg.StoreInterval, err = time.ParseDuration(configEVN.StoreInterval)
+		if err != nil {
+			cfg.StoreInterval = defaultStoreInterval
+		}
+	}
+
+	return cfg
 }

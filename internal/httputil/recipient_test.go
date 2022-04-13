381b8f29478cce9fcfe7ff36e8c58defa7fcdfea
Volkov Stanislav <sting.sk@gmail.com>
2022-04-13 20:47:42 +0300

    add tests

--- internal/httputil/recipient_test.go
+++ internal/httputil/recipient_test.go
@@ -186,25 +186,3 @@ func TestRecipientPost(t *testing.T) {
 		})
 	}
 }
-
-func TestMyMetric_savePostMetric(t *testing.T) {
-	type fields struct {
-		Inner *storage.MetricResourceMap
-	}
-	type args struct {
-		w http.ResponseWriter
-		r *http.Request
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			savePostMetric(tt.args.w, tt.args.r)
-		})
-	}
-}
--- internal/httputil/sender_test.go
+++ internal/httputil/sender_test.go
@@ -1,34 +1,8 @@
 package httputil
 
 import (
-	"context"
-	"sync"
 	"testing"
-	"time"
-
-	"github.com/Stingsk/Go_3_lesson_1/internal/metrics"
 )
-
-func TestRunSender(t *testing.T) {
-	type args struct {
-		ctx      context.Context
-		duration time.Duration
-		messages *metrics.SensorData
-		wg       *sync.WaitGroup
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			RunSender(tt.args.ctx, tt.args.duration, tt.args.messages, tt.args.wg, "localhost:8080")
-		})
-	}
-}
 
 func TestSend(t *testing.T) {
 	type args struct {
@@ -38,7 +12,12 @@ func TestSend(t *testing.T) {
 		name string
 		args args
 	}{
-		// TODO: Add test cases.
+		{
+			name: "positive test 1#",
+			args: args{
+				send: "dasdasd",
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
